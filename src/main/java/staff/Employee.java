00ac186113088e579c47e7070427d852326ab614
Colin Tindle <ampersand@underscore.space>
2018-05-17 14:16:42 +0100

    created Employee abstract class

---
+++ src/main/java/staff/Employee.java
@@ -0,0 +1,33 @@
+package staff;
+
+public abstract class Employee {
+    private String name;
+    private String niNumber;
+    private double salary;
+
+    Employee(String name, String niNumber, double salary) {
+        this.name = name;
+        this.niNumber = niNumber;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getNiNumber() {
+        return this.niNumber;
+    }
+
+    public double getSalary() {
+        return this.salary;
+    }
+
+    public void raiseSalary(double raiseAmount) {
+        this.salary += raiseAmount;
+    }
+
+    public double payBonus() {
+        return this.salary * 0.01;
+    }
+}
