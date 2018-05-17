8e84cff338a53f6df9d1cf0cf5646cc8554f0453
Colin Tindle <ampersand@underscore.space>
2018-05-17 15:11:34 +0100

    added Director class

---
+++ src/main/java/staff/management/Director.java
@@ -0,0 +1,19 @@
+package staff.management;
+
+public class Director extends Manager {
+
+    private double budget;
+
+    public Director (String name, String NiNumber, double salary, String deptName, double budget){
+
+
+        super(name, NiNumber, salary, deptName);
+        this.budget = budget;
+    }
+
+    public double getBudget(){
+
+        return this.budget;
+    }
+
+}
---
+++ src/test/java/staff/management/DirectorTest.java
@@ -0,0 +1,58 @@
+package staff.management;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DirectorTest {
+
+    private Director director;
+
+    @Before
+
+    public void before(){
+
+        director = new Director ("Helga", "3939393", 60000.00,
+            "accounts", 1000000.00);
+
+    }
+
+    @Test
+    public void canGetBudget() {
+        assertEquals(1000000.00, director.getBudget(), 0.01);
+    }
+
+    @Test
+    public void canGetName() {
+        assertEquals("Helga", director.getName());
+    }
+
+    @Test
+    public void canGetNiNumber() {
+        assertEquals("3939393", director.getNiNumber());
+    }
+
+    @Test
+    public void canGetSalary() {
+        assertEquals(60000.00, director.getSalary(), 0.01);
+    }
+
+    @Test
+    public void canRaiseSalary() {
+        director.raiseSalary(1);
+        assertEquals(60001.00, director.getSalary(), 0.01);
+    }
+
+    @Test
+    public void canGetPayBonus() {
+        assertEquals(600.00, director.payBonus(), 0.01);
+    }
+
+    @Test
+    public void canGetDeptName() {
+        assertEquals("accounts", director.getDeptName());
+    }
+
+
+}
